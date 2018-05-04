e3e10d849299af80b59f9c0190528a4741778d06
Alex Sosin <asosin@student.unimelb.edu.au>
2018-05-04 10:42:13 +1000

    Models changed

--- models.py
+++ models.py
@@ -16,34 +16,25 @@ def preproccess_data(data):
     print("Removal of punctuation: Done")
     data[6] = data[6].apply(lambda x: " ".join(x for x in x.split() if x not in stop))
     print("Removal of Stop words: Done")
-    data[6] = data[6].str.replace("urlLink", "")
+    data[6] = data[6].str.replace("urllink", "")
     print("Removal of urlLink: Done")
-    data[6] = data[6].apply(lambda x: str(TextBlob(x).correct()))
-    print("Spelling Check: Done")
+    #data[6] = data[6].apply(lambda x: str(TextBlob(x).correct()))
+    #print("Spelling Check: Done")
     data[6] = data[6].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
     print("Lemmatize: Done")
 
-    to16 = {"14": "13-16", "15": "13-16", "16": "13-16"}
-    to26 = {"24": "24-26", "25": "24-26", "26": "24-26"}
-    to36 = {"34": "34-36", "35": "34-36", "36": "34-36"}
-    to46 = {"44": "44-46", "45": "44-46", "46": "44-46"}
-
-    data[2] = multiple_replace(to16, data[2])
-    data[2] = multiple_replace(to26, data[2])
-    data[2] = multiple_replace(to36, data[2])
-    data[2] = multiple_replace(to46, data[2])
+    data[2] = data[2].replace([14, 15, 16], '14-16')
+    data[2] = data[2].replace([24, 25, 26], '24-26')
+    data[2] = data[2].replace([34, 35, 36], '34-36')
+    data[2] = data[2].replace([44, 45, 46], '44-46')
     print("Age Correct: Done")
-
-def multiple_replace(dict, text):
-    regex = re.compile("(%s)" % "|".join(map(re.escape, dict.keys())))
-    return regex.sub(lambda mo: dict[mo.string[mo.start():mo.end()]], text)
 
 def main():
     clean_data = Path('./data/train_clean.csv')
     if not clean_data.is_file():
         data = pd.read_csv('./data/train_raw.csv', header=None)
         preproccess_data(data)
-        data.to_csv('./data/train_clean.csv')
+        data.to_csv('./data/train_clean.csv', header=None)
     clean_train = pd.read_csv('./data/train_clean.csv', header=None)
     print(clean_train)
 
